eef698b5f97b2c3ef8261bfcce982109097f781e
HeitorMinuzzo <147281786+HeitorMinuzzo@users.noreply.github.com>
2023-10-07 19:11:40 -0300

    Add files via upload

---
+++ média.java
@@ -0,0 +1,9 @@
+public class media {
+    public static void main(String[] args) {
+    int valor1=2, valor2=3, valor3=10;
+    int somar;
+    somar=(valor1+valor2+valor3/3);
+    System.out.println("A média é: " +somar);
+    }
+
+}
