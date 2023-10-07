0604d09381fd840d9299d9ddfec4375686c32d52
HeitorMinuzzo <147281786+HeitorMinuzzo@users.noreply.github.com>
2023-10-07 19:19:47 -0300

    Add files via upload

---
+++ testandoSystem.out.java
@@ -0,0 +1,13 @@
+public class Gohan {
+
+    public static void main(String[] args) { 
+      int age=18;
+      String name ="gohan";
+      int paid=5000;
+      System.out.println("Nome é: " +name);
+      System.out.println("Salario é: " +paid);
+      System.out.println("Idade: " +age);
+     }
+
+
+}
