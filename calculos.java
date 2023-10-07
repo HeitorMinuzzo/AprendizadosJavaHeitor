356e932085a9518d953993c5fd5c0f47c29306eb
HeitorMinuzzo <147281786+HeitorMinuzzo@users.noreply.github.com>
2023-10-07 19:18:01 -0300

    Add files via upload

---
+++ calculos.java
@@ -0,0 +1,17 @@
+public class calculos {
+
+    public static void main(String[] args) {
+    	int soma, sub, multi, divisao;
+	int valor1=25;
+    	int valor2=30;
+      	soma=(valor1+valor2);
+      	sub=(valor1-valor2);
+      	multi=(valor1*valor2);
+      	divisao=(valor1/valor2);
+      	System.out.println("25+30 é: " +soma);
+      	System.out.println("25-30 é: " +sub);
+      	System.out.println("25 vezes 30 é: " +multi);
+      	System.out.println("25 dividido por 30 é: " +divisao);
+    }
+
+}
