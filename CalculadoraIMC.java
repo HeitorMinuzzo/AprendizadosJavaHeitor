6df9178a44e7f60cc007f4a0fd85a2dab5b3e90a
HeitorMinuzzo <147281786+HeitorMinuzzo@users.noreply.github.com>
2023-10-12 13:45:04 -0300

    Update CalculadoraIMC.java

--- CalculadoraIMC.java
+++ CalculadoraIMC.java
@@ -1,34 +1,35 @@
 import javax.swing.*;
-public class imc {
+public class aula {
 
     public static void main(String[] args) {
         double peso, altura, imc;
         peso=Double.parseDouble(JOptionPane.showInputDialog("Digite o peso"));
         altura=Double.parseDouble(JOptionPane.showInputDialog("Digite a altura"));
         imc=peso/(altura*altura);
+        
         System.out.println("O seu imc é: " +imc);
         
         if(imc<18.5) {
             JOptionPane.showMessageDialog(null,"Abaixo do peso");
             System.out.println("Abaixo do peso");
         }
-        else if (imc<25){
+        else if (imc<24.9){
             System.out.println("peso normal");
-            JOptionPane.showMessageDialog(null,"peso normal");
+            JOptionPane.showMessageDialog(null,"Peso Normal");
         }
-        else if(imc<30) {
+        else if(imc<29.9) {
             JOptionPane.showMessageDialog(null,"Sobrepeso");
             System.out.println("Sobrepeso");        
         }
-        else if(imc<35) {
-            JOptionPane.showMessageDialog(null,"obesidade grau 1");
-            System.out.println("obesidade grau 1");
+        else if(imc<34.9) {
+            JOptionPane.showMessageDialog(null,"Obesidade grau 1");
+            System.out.println("Obesidade grau 1");
         }
-         else if(imc<40) {
+         else if(imc<39.9) {
             JOptionPane.showMessageDialog(null,"Obesidade grau 2");
             System.out.println("Obesidade grau 2");
          }
-         else if(imc>40) {
+         else if(imc>=40) {
              JOptionPane.showMessageDialog(null,"Obesidade grau 3");
              System.out.println("Obesidade grau 3");
          }
