c21c2ae0cece66df573a00b8d09b75f2ae2aeb7f
HeitorMinuzzo <147281786+HeitorMinuzzo@users.noreply.github.com>
2023-10-12 13:08:03 -0300

    Add files via upload

---
+++ MédiaCom DadosDoUsuario.java
@@ -0,0 +1,13 @@
+import javax.swing.*; 
+public class medias {
+    public static void main(String[] args) {
+    int num1, num2, num3, media;
+    num1= Integer.parseInt(JOptionPane.showInputDialog("insira o primeiro valor"));
+    num2= Integer.parseInt(JOptionPane.showInputDialog("insira o segundo valor"));
+    num3= Integer.parseInt(JOptionPane.showInputDialog("insira o terceiro valor"));
+    media=(num1+num2+num3/3);
+    System.out.println("Sua média é: "+media);
+    JOptionPane.showMessageDialog(null,"Sua média é: "+media);
+    }
+
+}
