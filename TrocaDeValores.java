ce6aaa6638555f1dd3ca8f1b1212a8025ea859b6
HeitorMinuzzo <147281786+HeitorMinuzzo@users.noreply.github.com>
2023-10-12 13:13:00 -0300

    Add files via upload

---
+++ TrocaDeValores.java
@@ -0,0 +1,13 @@
+public class variavel2 {
+
+    public static void main(String[] args) {
+        int A=6, B=10, aux;
+        aux=A;
+        A=B;
+        B=aux;
+        System.out.println("A: "+A);
+        System.out.println("B: "+B);
+    }
+
+}
+//para trocar o valor A pelo B
