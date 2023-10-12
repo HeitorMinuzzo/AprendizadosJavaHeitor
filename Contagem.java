f8f59180ab95cdab28b1aed1976d033935b47726
HeitorMinuzzo <147281786+HeitorMinuzzo@users.noreply.github.com>
2023-10-12 13:49:58 -0300

    Add files via upload

---
+++ Contagem.java
@@ -0,0 +1,22 @@
+public class algoritimos {
+    
+    public static void main(String[] args) {
+        
+        int inicial= 100;
+        
+        
+        
+        for (int i = inicial; i <=1000;i++) { 
+            
+            
+            System.out.print("\\" + i + "\\");
+            
+        }
+        
+        
+        
+        
+        
+    }
+
+    }
